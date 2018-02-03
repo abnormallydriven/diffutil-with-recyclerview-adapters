a654695d385340611a4e01e444486dd5c76210e3
Brett <bretttucker@gmail.com>
2018-02-03 11:56:39 -0700

    Add initial project files
    This includes adding rxJava2 and recyclerview dependencies

---
+++ app/src/main/java/com/abnormallydriven/personlist/PeopleListActivity.kt
@@ -0,0 +1,12 @@
+package com.abnormallydriven.personlist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PeopleListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_people_list)
+    }
+}
